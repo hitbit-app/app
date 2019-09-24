9b940e520d8d35626fe7820448b4f5d8126e9c53
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2019-09-24 15:57:04 +0200

    chore: ...and that's how you do how you do linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,6 +56,6 @@ module.exports = {
     'unicode-bom': warn,
 
     'react-hooks/rules-of-hooks': error,
-    'react-hooks/exhaustive-deps': warn,
+    'react-hooks/exhaustive-deps': error,
   },
 };
