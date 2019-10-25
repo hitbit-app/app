8f9b386a0f15a0596f956700b92f97637a9f16aa
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2019-10-25 17:12:58 +0200

    feat: usePlayback hook
    In order to fix a known
    [problem](https://github.com/expo/expo/issues/1873)
    with SimpleExoPlayer on android `usePlayback` will create
    a single instance of `Audio.Sound` on play.
    
    This instance will be unloaded when another play is triggered
    to make sure a single instance exists.
    
    Known problems with current implementation:
    - When an unloaded audio is played again the sound file
      is downloaded again
    - Between play action and sound being reproduced there are
      no useful PlaybackStatusUpdate to show a buffer icon

--- components/AudioPlayer/AudioPlayer.js
+++ components/AudioPlayer/AudioPlayer.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import { Audio } from 'expo-av';
+import { PropTypes } from 'prop-types';
+import usePlayback from './usePlayback';
 
 import PauseIcon from '../../assets/icons/pauseIcon.svg';
 import PlayIcon from '../../assets/icons/playIcon.svg';
@@ -8,49 +9,33 @@ import BufferingIcon from '../../assets/icons/bufferingIcon.svg';
 
 import styles from '../../styles/audioPlayerStyle';
 
-Audio.setAudioModeAsync({
-  playsInSilentModeIOS: true,
-  allowsRecordingIOS: false,
-  staysActiveInBackground: true,
-  interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-  shouldDuckAndroid: false,
-  playThroughEarpieceAndroid: true,
-  interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-});
-
 export default function AudioPlayer(props) {
-  const [sound, setSound] = useState(null);
-  const [status, setStatus] = useState(null);
+  const { sound, status } = usePlayback({ uri: props.source });
+  const { isPlaying, shouldPlay } = status;
 
-  useEffect(() => {
-    Audio.Sound.createAsync({ uri: props.source }).then(({ sound, status }) => {
-      setSound(sound);
-      setStatus(status);
-    });
-  }, [props.source]);
+  let stateIcon;
 
-  if (sound) {
-    sound.setOnPlaybackStatusUpdate(setStatus);
+  if (isPlaying) {
+    stateIcon = <PauseIcon height={20} width={20} />;
+  } else if (shouldPlay) {
+    stateIcon = <BufferingIcon height={20} width={20} />;
+  } else {
+    stateIcon = <PlayIcon height={20} width={20} />;
   }
-
-  const playAudio = () => sound && sound.playAsync();
-  const pauseAudio = () => sound && sound.pauseAsync();
-  const isPlaying = () => status && status.isPlaying;
-  const isBuffering = () => status && status.isBuffering;
 
   return (
     <View style={styles.container}>
       <Text>{props.author}</Text>
       <TouchableOpacity
-        onPress={() => (isPlaying() ? pauseAudio() : playAudio())}
+        onPress={() => isPlaying ? sound.pauseAsync() : sound.playAsync()}
       >
-        {isBuffering() && !isPlaying() && <BufferingIcon height={20} width={20} />}
-        {isPlaying()
-          ? <PauseIcon height={20} width={20} />
-          : <PlayIcon height={20} width={20} />
-        }
-
+        {stateIcon}
       </TouchableOpacity>
     </View>
   );
 }
+
+AudioPlayer.propTypes = {
+  source: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
