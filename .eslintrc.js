f40b44cba170d54fe3c350d30b056707fdcde69c
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2019-11-06 14:55:56 +0100

    chore: use @react-native-community/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,7 @@ module.exports = {
   'extends': [
     'eslint:recommended',
     'plugin:react/recommended',
-  ],
-
-  'plugins': [
-    'react',
-    'react-hooks',
+    '@react-native-community',
   ],
 
   'parserOptions': {
